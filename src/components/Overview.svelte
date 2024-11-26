046d1dd742b4a5a427b01c252eb3d0ca12ffa365
victorfaruna <farunavictor043@gmail.com>
2024-11-26 19:02:00 +0100

    coool dude

--- src/components/Overview.svelte
+++ src/components/Overview.svelte
@@ -5,7 +5,7 @@
 	<br /><span class="text-[2.5rem]">Overview</span>
 </p>
 <div
-	class="overview-cont no-scrollbar w-full h-[600px] md:h-[450px] flex flex-nowrap gap-5 overflow-x-auto mb-20 pl-[10%] md:pl-10"
+	class="overview-cont no-scrollbar w-full h-[550px] md:h-[450px] flex flex-nowrap gap-5 overflow-x-auto mb-20 pl-[10%] md:pl-10"
 >
 	<div
 		class="item relative overflow-hidden w-[30%] md:w-[80%] h-full rounded-[2rem] bg-[url(/images/project-exhibition.webp)] bg-cover bg-no-repeat flex flex-col justify-end"
@@ -30,6 +30,19 @@
 				JOJI DAO is offering up to 10 exclusive scholarship slots to support talented individuals
 				passionate about blockchain and Web3. Don’t miss this chance to learn, grow, and innovate
 				with the best in the industry!
+			</p>
+		</div>
+	</div>
+
+	<div
+		class="item relative overflow-hidden w-[30%] md:w-[80%] h-full rounded-[2rem] bg-[url(/images/merches.webp)] bg-cover bg-no-repeat flex flex-col justify-end"
+		style="flex: 0 0 auto"
+	>
+		<div class="inner w-full p-5 flex flex-col gap-4 relative z-[2]">
+			<p class="title">Merches and Incentives Giveaways</p>
+			<p class="desc">
+				Exciting rewards await! Grab exclusive JOJI DAO merch and win incentive giveaways as we
+				celebrate innovation and community in the Web3 space. Don’t miss out!
 			</p>
 		</div>
 	</div>
