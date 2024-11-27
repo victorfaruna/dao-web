573172c3bd725b8a141ee525599fd11012775910
victorfaruna <farunavictor043@gmail.com>
2024-11-27 18:24:13 +0100

    cool

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -1,4 +1,18 @@
-<div id="about" class="w-full px-[10%] md:px-8 flex md:flex-col-reverse gap-10 font-orbitron">
+<script lang="ts">
+	import { inview } from 'svelte-inview';
+	import type { ObserverEventDetails, Options } from 'svelte-inview';
+	let isInView: boolean;
+	const options: Options = {
+		unobserveOnEnter: true
+	};
+</script>
+
+<div
+	use:inview={options}
+	on:inview_change={({ detail }: CustomEvent<ObserverEventDetails>) => (isInView = detail.inView)}
+	id="about"
+	class="w-full px-[10%] md:px-8 flex md:flex-col-reverse gap-10 font-orbitron"
+>
 	<div class="left w-[40%] md:w-full flex justify-center">
 		<div class="img relative w-[350px] h-[450px] p-[0.18rem] xl:w-[90%]">
 			<img src="/images/cana.jpeg" class="mask mask-squircle size-full object-cover" alt="" />
@@ -6,11 +20,15 @@
 	</div>
 	<div class="right w-[60%] md:w-full flex flex-col gap-5">
 		<p class="text-color-1 text-[3rem] md:text-[2rem] text-orange-300 font-bold">
-			About <img class="inline w-[100px]" src="/images/coooo.svg" alt="" /> <br /><span
-				class="text-[2.5rem]">Joji Dao</span
-			>
+			About <img
+				class={`dx inline w-[100px] ${isInView ? 'translate-x-0 transition-all duration-[1s]' : 'translate-x-[100px]'}`}
+				src="/images/coooo.svg"
+				alt=""
+			/> <br /><span class="text-[2.5rem]">Joji Dao</span>
 		</p>
-		<p class="text-[0.9rem] md:text-[0.7rem] font-sora">
+		<p
+			class={`text-[0.9rem] md:text-[0.7rem] font-sora ${isInView ? 'translate-y-0 transition-all duration-[1s]' : 'translate-y-[50px]'}`}
+		>
 			Joji Dao is a community that is built for helping Crypto projects grow in Nigeria by hosting
 			IRL events and online community development. <br /><br />The first active Crypto community in
 			Ekiti State built for onboarding and empowering individuals in Crypto and Web3. <br /><br />We
