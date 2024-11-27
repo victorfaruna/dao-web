d7ea79ebd69bbeda32dbdd262b8dfaba7838a1f4
victorfaruna <farunavictor043@gmail.com>
2024-11-27 18:31:29 +0100

    cool

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -19,12 +19,12 @@
 		</div>
 	</div>
 	<div class="right w-[60%] md:w-full flex flex-col gap-5">
-		<p class="text-color-1 text-[3rem] md:text-[2rem] text-orange-300 font-bold">
-			About <img
-				class={`dx inline w-[100px] ${isInView ? 'translate-x-0 transition-all duration-[1s]' : 'translate-x-[100px]'}`}
-				src="/images/coooo.svg"
-				alt=""
-			/> <br /><span class="text-[2.5rem]">Joji Dao</span>
+		<p
+			class={`text-color-1 text-[3rem] md:text-[2rem] text-orange-300 font-bold${isInView ? 'translate-x-0 transition-all duration-[1s]' : 'translate-x-[100px]'}`}
+		>
+			About <img class="dx inline w-[100px]" src="/images/coooo.svg" alt="" /> <br /><span
+				class="text-[2.5rem]">Joji Dao</span
+			>
 		</p>
 		<p
 			class={`text-[0.9rem] md:text-[0.7rem] font-sora ${isInView ? 'translate-y-0 transition-all duration-[1s]' : 'translate-y-[50px]'}`}
