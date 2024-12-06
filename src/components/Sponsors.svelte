ba8ceca1ff221f027816e9c5eb73e2718b5663c5
victorfaruna <farunavictor043@gmail.com>
2024-12-06 14:20:40 +0100

    cool

--- src/components/Sponsors.svelte
+++ src/components/Sponsors.svelte
@@ -16,53 +16,22 @@
 			link: 'https://qiibeefoundation.org/'
 		}
 	]);
-
-	$effect(() => {
-		setInterval(() => {
-			sponsors.push({
-				name: 'Offchain',
-				image: '/images/offchain.webp',
-				link: ''
-			});
-			sponsors.push({
-				name: 'Core',
-				image: '/images/core.webp',
-				link: ''
-			});
-			sponsors.push({
-				name: 'QBX',
-				image: '/images/qbx.webp',
-				link: ''
-			});
-		}, 800);
-	});
-
-	let scrollContainer: any;
-	function autoScroll() {
-		scrollContainer.scrollLeft += 10;
-		requestAnimationFrame(autoScroll); // Smooth looping
-	}
-	$effect(() => {
-		autoScroll();
-	});
 </script>
 
 <div class="my-20" id="about">
 	<p class="text-[2.3rem] md:text-[1.3rem] text-center font-sora font-semibold mb-10">
 		Partners & Sponsors 🤝
 	</p>
-	<div
-		bind:this={scrollContainer}
-		class="marquee no-scrollbar w-full flex flex-nowrap overflow-x-auto gap-20 items-center scroll-smooth"
-	>
-		{#each sponsors as sponsor}
-			<a href={sponsor.link} target="_blank" style="flex: 0 0 auto">
-				<img
-					src={sponsor.image}
-					alt={sponsor.name}
-					class="w-[140px] md:w-[90px] h-auto object-cover"
-				/>
-			</a>
-		{/each}
+	<div class="relative flex overflow-hidden gap-20">
+		<div class="py-5 animate-marquee whitespace-nowrap flex items-center gap-20">
+			{#each sponsors as sponsor}
+				<a href={sponsor.link}><img class="w-[130px]" src={sponsor.image} alt="" /></a>
+			{/each}
+		</div>
+		<div class="absolute top-0 py-5 animate-marquee2 whitespace-nowrap flex items-center gap-20">
+			{#each sponsors as sponsor}
+				<a href={sponsor.link}><img class="w-[150px]" src={sponsor.image} alt="" /></a>
+			{/each}
+		</div>
 	</div>
 </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -32,6 +32,20 @@ export default {
 				nunito: ['Nunito', 'sans-serif'],
 				varela: ['Varela Round', 'sans-serif'],
 				'nova-round': 'Nova Round'
+			},
+			animation: {
+				marquee: 'marquee 25s linear infinite',
+				marquee2: 'marquee2 25s linear infinite'
+			},
+			keyframes: {
+				marquee: {
+					'0%': { transform: 'translateX(0%)' },
+					'100%': { transform: 'translateX(-100%)' }
+				},
+				marquee2: {
+					'0%': { transform: 'translateX(100%)' },
+					'100%': { transform: 'translateX(0%)' }
+				}
 			}
 		}
 	},
