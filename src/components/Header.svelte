eea4542127eb4aa7dd319833a9bcb278671a4582
victorfaruna <farunavictor043@gmail.com>
2024-12-07 12:55:10 +0100

    cool

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -19,6 +19,7 @@
 >
 	<div class="rest w-full h-screen flex gap-4 flex-col items-center justify-center">
 		<img src="/images/undraw_bitcoin_2.svg" class="w-[50%]" alt="" />
+
 		<div class="socials flex gap-2 items-center justify-center text-main">
 			<button
 				onclick={() => window.open('//x.com/JOJI_INT', '_blank')}
@@ -75,6 +76,23 @@
 				</svg>
 			</button>
 		</div>
+		<a href="/scholarship" class="flex gap-1 text-main">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				class="size-6"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5"
+				/>
+			</svg>
+			Apply for schoolarship
+		</a>
 	</div>
 </section>
 
