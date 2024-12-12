f78330ef23cd75e09dc23b70c8fae2cb95e9c700
victorfaruna <farunavictor043@gmail.com>
2024-12-12 15:49:49 +0100

    cool

--- src/lib/server/instagram.ts
+++ src/lib/server/instagram.ts
@@ -5,7 +5,7 @@ export async function isFollowing(username: any) {
 		method: 'POST',
 		url: 'https://rocketapi-for-instagram.p.rapidapi.com/instagram/user/get_followers',
 		headers: {
-			'x-rapidapi-key': '8d7145d901msh18a8d29b7a15495p10c346jsnba409010d96e',
+			'x-rapidapi-key': 'aff5f3a285msh302fe6d973fbec4p16c7b8jsne4750235d686',
 			'x-rapidapi-host': 'rocketapi-for-instagram.p.rapidapi.com',
 			'Content-Type': 'application/json'
 		},
@@ -35,7 +35,7 @@ export async function hasEngaged(username: any) {
 		method: 'POST',
 		url: 'https://rocketapi-for-instagram.p.rapidapi.com/instagram/media/get_likes',
 		headers: {
-			'x-rapidapi-key': '8d7145d901msh18a8d29b7a15495p10c346jsnba409010d96e',
+			'x-rapidapi-key': 'aff5f3a285msh302fe6d973fbec4p16c7b8jsne4750235d686',
 			'x-rapidapi-host': 'rocketapi-for-instagram.p.rapidapi.com',
 			'Content-Type': 'application/json'
 		},
--- src/routes/scholarship/+page.svelte
+++ src/routes/scholarship/+page.svelte
@@ -201,39 +201,26 @@
 					}
 					break;
 				case 2:
-					if (item === 2) {
+					if (twitterUsername && instagramUsername) {
 						if (!isTaskCheckingLoadingList.includes(item)) {
 							isTaskCheckingLoadingList.push(item);
 						}
-						setTimeout(() => {
+						const { data } = await axios.get(
+							'/api/instagram/verify-follow?targetUsername=' + instagramUsername
+						);
+						if (data.followStatus === true) {
 							addConfirmedTask(item);
+							showAlert('Task completed', 'alert-success');
+						} else {
 							isTaskCheckingLoadingList = isTaskCheckingLoadingList.filter(
 								(item: any) => item !== item
 							);
-							showAlert('Task completed', 'alert-success');
-						}, 2000);
+							showAlert('Task failed - Check your username', 'alert-error');
+						}
+					} else {
+						modalPopupSocial.showModal();
 					}
 					break;
-				// if (twitterUsername && instagramUsername) {
-				// 	if (!isTaskCheckingLoadingList.includes(item)) {
-				// 		isTaskCheckingLoadingList.push(item);
-				// 	}
-				// 	const { data } = await axios.get(
-				// 		'/api/instagram/verify-follow?targetUsername=' + instagramUsername
-				// 	);
-				// 	if (data.followStatus === true) {
-				// 		addConfirmedTask(item);
-				// 		showAlert('Task completed', 'alert-success');
-				// 	} else {
-				// 		isTaskCheckingLoadingList = isTaskCheckingLoadingList.filter(
-				// 			(item: any) => item !== item
-				// 		);
-				// 		showAlert('Task failed - Check your username', 'alert-error');
-				// 	}
-				// } else {
-				// 	modalPopupSocial.showModal();
-				// }
-				// break;
 				case 3:
 					if (item === 3) {
 						if (!isTaskCheckingLoadingList.includes(item)) {
@@ -249,39 +236,26 @@
 					}
 					break;
 				case 4:
-					if (item === 4) {
+					if (twitterUsername && instagramUsername) {
 						if (!isTaskCheckingLoadingList.includes(item)) {
 							isTaskCheckingLoadingList.push(item);
 						}
-						setTimeout(() => {
+						const { data } = await axios.get(
+							'/api/instagram/verify-engagement?targetUsername=' + instagramUsername
+						);
+						if (data.engagementStatus === true) {
 							addConfirmedTask(item);
+							showAlert('Task completed', 'alert-success');
+						} else {
 							isTaskCheckingLoadingList = isTaskCheckingLoadingList.filter(
 								(item: any) => item !== item
 							);
-							showAlert('Task completed', 'alert-success');
-						}, 2000);
+							showAlert('Task failed - Check username & confirm post engagement', 'alert-error');
+						}
+					} else {
+						modalPopupSocial.showModal();
 					}
 					break;
-				// if (twitterUsername && instagramUsername) {
-				// 	if (!isTaskCheckingLoadingList.includes(item)) {
-				// 		isTaskCheckingLoadingList.push(item);
-				// 	}
-				// 	const { data } = await axios.get(
-				// 		'/api/instagram/verify-engagement?targetUsername=' + instagramUsername
-				// 	);
-				// 	if (data.engagementStatus === true) {
-				// 		addConfirmedTask(item);
-				// 		showAlert('Task completed', 'alert-success');
-				// 	} else {
-				// 		isTaskCheckingLoadingList = isTaskCheckingLoadingList.filter(
-				// 			(item: any) => item !== item
-				// 		);
-				// 		showAlert('Task failed - Check username & confirm post engagement', 'alert-error');
-				// 	}
-				// } else {
-				// 	modalPopupSocial.showModal();
-				// }
-				// break;
 				case 5:
 					if (item === 5) {
 						if (!isTaskCheckingLoadingList.includes(item)) {
